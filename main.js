484251d83f09926c10657527d2654c16b8674b42
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-03-08 14:30:29 -0500

    barrera1994/Objetos_propiedades_metodos_24_02_2021

---
+++ main.js
@@ -0,0 +1,25 @@
+const Contrusctores = new Object({
+    Contrusctores_obj : function(){
+        this.nombre;
+        this.edad;
+        return this;
+    }
+});
+const settters = new Object({
+    settter_obj : function(){
+        this.nombre;
+        this.edad;
+        return this;
+    }
+});
+
+const Contrusctores_obj = Contrusctores.Contrusctores_obj.bind({nombre:'Julian', edad: 27});
+const obj = new Object();
+obj.__proto__.construnctor = Contrusctores_obj();
+obj.__defineSetter__('setNombre', function(val) { obj.__proto__.construnctor.nombre = val; });
+obj.__defineGetter__('getNombre', function() { return obj.__proto__.construnctor.nombre; });
+Object.seal(obj);
+
+obj.setNombre = "Marcela";
+console.log(obj.getNombre);
+console.log(obj);
